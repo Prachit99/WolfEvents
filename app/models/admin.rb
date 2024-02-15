9fb89302263144ae8cc61824030821d6003620df
Prachit Mhalgi <prachitmhalgi@gmail.com>
2024-02-15 00:10:31 -0500

    added admin record

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,4 @@
 class Admin < ApplicationRecord
-  has_secure_password
-
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, uniqueness: true
